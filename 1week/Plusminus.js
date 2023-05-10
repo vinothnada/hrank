cab860499a0733b19e69df2277e2288ad1165c7b
vinothnada <vinonada@gmail.com>
2023-05-10 11:35:16 +0530

    1week

---
+++ 1week/Plusminus.js
@@ -0,0 +1,56 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  // Write your code here
+  let [positive, negative, zero] = [0, 0, 0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zero++;
+    } else if (arr[i] > 0) {
+      positive++;
+    } else {
+      negative++;
+    }
+  }
+
+  console.log((positive / arr.length).toFixed(6));
+  console.log((negative / arr.length).toFixed(6));
+  console.log((zero / arr.length).toFixed(6));
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
